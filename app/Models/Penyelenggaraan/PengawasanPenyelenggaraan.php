e335fb502cb6fa1b8c25ee378937719188ffb1d7
qqdnada <nada.qori@gmail.com>
2024-08-17 15:09:02 +0800

    feat(pengawasan-penyelenggaraan): pengawasan penyelenggaraan table and model

---
+++ app/Models/Penyelenggaraan/PengawasanPenyelenggaraan.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models\Penyelenggaraan;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class PengawasanPenyelenggaraan extends Model
+{
+    use HasUuids, SoftDeletes;
+
+    protected $table = 'pengawasan_penyelenggaraan_konstruksi';
+
+    protected $fillable = [
+        'jenis_pengawasan',
+        'tanggal_pengawasan',
+        'proyek_konstruksi_id',
+        'tertib_proses_pemilihan_penyedia_jasa',
+        'tertib_penerapan_standar_kontrak',
+        'tertib_penggunaan_tkk',
+        'tertib_pemberian_pekerjaan',
+        'tertib_ketersediaan_dokumen_standar_k4',
+        'tertib_penerapan_smkk',
+        'tertib_antisipasi_kecelakaan',
+        'tertib_penerapan_manajemen_mutu',
+        'tertib_pemenuhan_penyediaan_mptk',
+        'tertib_penggunaan_mptk',
+        'tertib_penggunaan_pdn',
+        'tertib_pemenuhan_standar_lingkungan',
+        'tertib_pengawasan',
+        'catatan',
+        'verified_by',
+        'verified_at',
+        'created_by',
+    ];
+
+    public function proyekKonstruksi(): BelongsTo
+    {
+        return $this->belongsTo(ProyekKonstruksi::class, 'proyek_konstruksi_id');
+    }
+}

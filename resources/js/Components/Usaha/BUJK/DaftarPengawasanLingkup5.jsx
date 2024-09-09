19fb29a8307bcb64fcba2dea112659e4a8ee14af
qqdnada <nada.qori@gmail.com>
2024-09-09 08:45:38 +0800

    feat(pengawasan-usaha): pengawasan bujk lingkup 5 - index

--- resources/js/Components/Usaha/BUJK/DaftarPengawasanLingkup5.jsx
+++ resources/js/Components/Usaha/BUJK/DaftarPengawasanLingkup5.jsx
@@ -9,6 +9,13 @@ import formatDateToIndonesia from "../../../Utils/formatDateToIndonesia";
 import { LiaSearchSolid } from "react-icons/lia";
 
 export default ({ daftarPengawasan }) => {
+    const [keyword, setKeyword] = React.useState('');
+    const handleKeywordChange = (event) => setKeyword(event.target.value);
+
+    const filteredDaftarPengawasan = keyword ? daftarPengawasan.filter(({ usaha }) => {
+        return usaha.nama.toLowerCase().includes(keyword.toLowerCase());
+    }) : daftarPengawasan;
+
     return (
         <Card>
             <Card.Header className="space-y-4">
@@ -25,7 +32,7 @@ export default ({ daftarPengawasan }) => {
                             className="px-3 py-2 block w-full rounded-md border-slate-200 text-slate-600 placeholder:text-slate-500 focus:ring-blue-400 focus:border-blue-400 text-xs"
                         >
                             <option>2024</option>
-                            <option>2023</option>
+                            {/* <option>2023</option> */}
                         </select>
                     </div>
                     <div className="relative mx-2">
@@ -33,7 +40,7 @@ export default ({ daftarPengawasan }) => {
                             <LiaSearchSolid size={18} className="text-slate-500 -scale-x-100" />
                         </div>
                         <input
-                            type="search" name="search" placeholder="Cari..."
+                            type="search" name="search" placeholder="Cari..." value={keyword} onChange={handleKeywordChange}
                             className="border border-slate-200 rounded py-2 pl-8 block w-56 text-slate-700 placeholder:text-slate-400 focus:ring-blue-400 focus:border-blue-400 text-xs"
                         />
                     </div>
@@ -43,16 +50,17 @@ export default ({ daftarPengawasan }) => {
                 <div className="relative overflow-x-auto">
                     <table className="w-full text-xs">
                         <thead className="bg-slate-50 border-b border-slate-100 text-slate-500 uppercase">
-                            <tr>
-                                <th scope="col" className="p-4 font-medium">#</th>
-                                <th scope="col" className="p-4 font-medium min-w-60">Nama Badan Usaha</th>
-                                <th scope="col" className="p-4 font-medium min-w-40">Tanggal Pengawasan</th>
-                                <th scope="col" className="p-4 font-medium">Pelaksanaan Pengembangan Usaha Berkelanjutan</th>
+                            <tr className="border-b border-slate-200">
+                                <th scope="col" className="p-4 font-medium border-r border-slate-200">#</th>
+                                <th scope="col" className="p-4 font-medium min-w-60 border-r border-slate-200">Nama Badan Usaha</th>
+                                <th scope="col" className="p-4 font-medium min-w-40 border-r border-slate-200">Tanggal Pengawasan</th>
+                                <th scope="col" className="p-4 font-medium border-r border-slate-200">Pelaksanaan Pengembangan <br /> Usaha Berkelanjutan</th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody className="text-slate-700">
                         {
-                            daftarPengawasan.map((pengawasan, i) => (
+                            filteredDaftarPengawasan.map((pengawasan, i) => (
                             <tr key={pengawasan.id} className="border-b border-slate-100 hover:bg-slate-50">
                                 <td className="px-4 py-5 text-center">{i + 1}</td>
                                 <td className="px-4 py-5">
@@ -65,6 +73,16 @@ export default ({ daftarPengawasan }) => {
                                 </td>
                                 <td className="px-4 py-5 text-center">{formatDateToIndonesia(new Date(pengawasan.tanggalPengawasan))}</td>
                                 <td className="px-4 py-5 text-center">{getTertibStatusBadge(pengawasan.tertibPengembanganUsaha)}</td>
+                                <td className="px-4 py-5">
+                                    <div className="flex justify-end gap-x-2">
+                                        <Link
+                                            href={`/admin/pengawasan/usaha/5/${pengawasan.id}`}
+                                            className="flex items-center gap-x-1 rounded border border-slate-200 text-slate-500 px-2.5 py-1.5 hover:bg-slate-200"
+                                            >
+                                            Lihat
+                                        </Link>
+                                    </div>
+                                </td>
                             </tr>
                             ))
                         }

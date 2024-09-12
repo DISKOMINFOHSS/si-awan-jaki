df85dc26b7e60576979421ac8227a14d914bd996
qqdnada <nada.qori@gmail.com>
2024-09-12 12:05:31 +0800

    feat(pengawasan-penyelenggaraan): simak pengawasan rutin tertib penyelenggaraan pdf

--- resources/js/Pages/Pengawasan/Penyelenggaraan/APBD/Rutin/Simak.jsx
+++ resources/js/Pages/Pengawasan/Penyelenggaraan/APBD/Rutin/Simak.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Font, Document, PDFViewer, Page, View, Text, Svg } from "@react-pdf/renderer";
+import { Font, Document, PDFViewer, Page, View, Text } from "@react-pdf/renderer";
 import { createTw } from "react-pdf-tailwind";
 
 import { inter } from "../../../../../Utils/fonts";
@@ -28,11 +28,11 @@ export default ({ data }) => {
         <PDFViewer width="100%" style={tw("min-h-screen")}>
             <Document title="simak-pengawasan-tertib-penyelenggaraan-apbd-rutin">
                 <Page wrap size="A4" orientation="landscape" style={tw("p-12 font-sans relative")}>
-                    <View style={tw("font-medium text-center text-[12px]")}>
+                    <View style={tw("font-medium text-center text-[12px] h-[7%]")}>
                         <Text>Simak - Pengawasan Tertib Penyelenggaraan Konstruksi secara Rutin</Text>
                         <Text>Penyelenggaraan Konstruksi dengan Sumber Dana dari APBD</Text>
                     </View>
-                    <View style={tw("mt-6 mb-4 text-[11px]")}>
+                    <View style={tw("my-4 text-[11px] h-[28%]")}>
                         <View style={tw("flex flex-row gap-1 mb-1")}>
                             <Text style={tw("w-[26%]")}>Nama Proyek Konstruksi</Text>
                             <Text style={tw("w-[1%]")}>:</Text>
@@ -71,8 +71,8 @@ export default ({ data }) => {
                             <Text style={tw("w-[73%]")}>{formatDateToIndonesia(pengawasan.tanggalPengawasan)}</Text>
                         </View>
                     </View>
-                    <View>
-                        <View fixed style={tw("flex flex-row w-full text-[10px] font-bold text-center")}>
+                    <View style={tw("min-h-[65%]")}>
+                        <View fixed style={tw("flex flex-row w-full text-[10px] font-bold text-center min-h-8")}>
                             <View style={tw("w-[5%] border-y border-l border-slate-800 p-2")}>
                                 <Text style={tw("text-center")}>NO</Text>
                             </View>
@@ -89,7 +89,7 @@ export default ({ data }) => {
                         </View>
                         {
                             daftarLingkupPengawasan.map((lingkup, i) => (
-                                <View wrap={false} key={i} style={tw("flex flex-row w-full text-[10px]")}>
+                                <View wrap={false} key={i} style={[tw("flex flex-row w-full text-[10px] min-h-64")]}>
                                     <View style={tw("w-[5%] border-b border-l border-slate-800 p-2")}>
                                         <Text style={tw("text-center")}>{i + 1}.</Text>
                                     </View>
@@ -97,29 +97,29 @@ export default ({ data }) => {
                                         <View style={tw("flex flex-row w-full")}>
                                             <View style={tw("w-[30%] border-r border-slate-800")}>
                                                 <Text style={tw("p-2")}>{lingkup.lingkupPengawasan}</Text>
-                                                <View style={tw("flex flex-row border-t border-slate-800 w-full mt-1")}>
+                                                <View style={tw("flex flex-row border-t border-slate-800 w-full mt-1 h-full")}>
                                                     <Text style={tw("w-[27.5%] border-r border-slate-800 p-2")}>Indikator :</Text>
                                                     <View style={tw("w-[72.5%] flex flex-col")}>
                                                     {
                                                         lingkup.indikator.map(({ id, indikator }, j) => (
-                                                            <View key={id}>
+                                                            <React.Fragment key={id}>
                                                                 <View style={tw("flex flex-row w-full p-2")}>
                                                                     { id[1] && <Text style={tw("w-[10%]")}>{id[1]}.</Text> }
                                                                     <Text style={tw("grow w-full")}>{indikator}</Text>
                                                                 </View>
                                                                 { j+1 !== lingkup.indikator.length && <View key={id} style={tw("w-full border-b border-slate-800")}></View> }
-                                                            </View>
+                                                            </React.Fragment>
                                                         ))
                                                     }
                                                     </View>
                                                 </View>
                                             </View>
                                             <Text style={tw("w-[22.5%] border-r border-slate-800 p-2")}>{lingkup.dokumen}</Text>
-                                            <View style={tw("w-[47.5%]")}>
+                                            <View style={tw("w-[47.5%] h-full")}>
                                                 {
                                                     lingkup.daftarPemeriksaan.map(({ label, caraPemeriksaan, catatan, kesimpulan }, j) => (
                                                         <React.Fragment key={j}>
-                                                            <View  style={tw("flex flex-row")}>
+                                                            <View  style={tw("flex flex-row h-1/2")}>
                                                                 <Text style={tw("w-[40%] border-r border-slate-800 p-2")}>{caraPemeriksaan}</Text>
                                                                 <View style={tw("w-[60%] p-2")}>
                                                                     <View style={tw("flex flex-row items-center gap-2")}>
@@ -145,7 +145,7 @@ export default ({ data }) => {
                     </View>
                     <Text
                         render={({ pageNumber }) => (`- ${pageNumber} -`)} fixed
-                        style={tw("absolute bottom-8 right-0 left-0 text-[9px] text-center")}
+                        style={tw("absolute bottom-7 right-0 left-0 text-[9px] text-center")}
                     />
                 </Page>
             </Document>

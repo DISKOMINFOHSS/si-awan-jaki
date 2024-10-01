a7f07a730d452ee5d869842fb3363d38e1486828
qqdnada <nada.qori@gmail.com>
2024-10-01 16:00:48 +0800

    feat(pendataan-usaha): show usaha rantai pasok

--- app/Http/Controllers/Pendataan/Usaha/RantaiPasokController.php
+++ app/Http/Controllers/Pendataan/Usaha/RantaiPasokController.php
@@ -4,12 +4,27 @@ namespace App\Http\Controllers\Pendataan\Usaha;
 
 use Inertia\Inertia;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Pendataan\UsahaRantaiPasokResource;
+use App\Services\Usaha\PendataanUsahaService;
 use Illuminate\Http\Request;
 
 class RantaiPasokController extends Controller
 {
+    protected $usahaService;
+
+    public function __construct(PendataanUsahaService $usahaService)
+    {
+        $this->usahaService = $usahaService;
+    }
+
     public function show(string $id)
     {
-        return Inertia::render('Pendataan/Usaha/RantaiPasok/Show');
+        $usaha = $this->usahaService->getUsahaById($id);
+
+        return Inertia::render('Pendataan/Usaha/RantaiPasok/Show', [
+            'data' => [
+                'usaha' => new UsahaRantaiPasokResource($usaha),
+            ],
+        ]);
     }
 }
---
+++ app/Http/Resources/Pendataan/UsahaRantaiPasokResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\Pendataan;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+
+class UsahaRantaiPasokResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'                  => $this->id,
+            'nama'                => $this->nama,
+            'nib'                 => $this->nib,
+            'dokumenNIB'          => $this->when($this->dokumen_nib, [
+                'fileId'   => $this->dokumen_nib,
+                'fileName' => $this->name,
+                'filePath' => Storage::url($this->path),
+            ]),
+            'pjbu'                => $this->pjbu,
+            'alamat'              => $this->alamat,
+            'jenisUsaha'          => $this->whenLoaded('jenisUsaha'),
+        ];
+    }
+}
